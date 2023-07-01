da7dc5599aa1e85009ee7cb67abf0e826bf0e7f2
Nykuluz <carlmillanes3@gmail.com>
2023-07-02 03:28:31 +0800

    Initial Commit

---
+++ L2 Demo/L2_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L2_DemoApp.swift
+//  L2 Demo
+//
+//  Created by Carl Irven Nykuluz F. Millanes on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct L2_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
